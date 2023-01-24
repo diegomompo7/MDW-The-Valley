49c1e568f37652e3175860e1164ead326c8a9db1
diegomompo <diego@diegomompo.com>
2023-01-24 14:58:36 +0100

    Tarea10_Edad

--- Tarea10_AdivinaTuEdad/main.js
+++ Tarea10_AdivinaTuEdad/main.js
@@ -1,13 +1,14 @@
-let nombre = document.getElementById('name')
-function adivinaEdad(nom){
-  const URL = "https://api.agify.io?name=" + nom;
+
+function adivinaEdad(){
+  let nombre = document.getElementById('name').value
+  console.log(nombre)
+  const URL = "https://api.agify.io?name=" + nombre;
 
   fetch(URL)
     .then(respuesta => respuesta.json())
-    .then(respuesta => console.log(respuesta.data))
+    .then(respuesta => document.getElementById("demo").innerHTML = respuesta.age)
     .catch(error => console.log(error))
 
 }
 
-console.log(nombre)
-document.getElementById("adivinar").addEventListener("click" , () => adivinaEdad(nombre))
+document.getElementById("adivinar").addEventListener("click" , () => adivinaEdad())
