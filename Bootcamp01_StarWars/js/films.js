673d2e37fa58431d2f2f1067dcb1efd413271803
diegomompo <diego@diegomompo.com>
2023-01-31 19:35:57 +0100

    Bootcamp01_StarWars 31-01

--- Bootcamp01_StarWars/js/films.js
+++ Bootcamp01_StarWars/js/films.js
@@ -4,98 +4,69 @@ const getFilms = async() => {
     let url = URL_BASE + "/films/";
     let response = await fetch(url)
     let data = await response.json();
-
+    data = mapDataFilms(data.results)
     return data;
 }
 
+const mapDataFilms = (data) =>{
+    let dataMapped = data.map(film => {
+        let object = {
+            title: film.title,
+            img: "assets/images/films/" + film.url.replace("https://swapi.dev/api/films/", "").replace("/", "") + ".jpg",
+            episode: getRomanNumber(film.episode_id),
+            director: film.director,
+            date: film.release_date,
+            urlDetail: film.url,
+        }
+
+        return object
+    });
+
+    return dataMapped
+}
+const formatFilmsCards = (films) => {
+    let templatesFilms = films.map(film => {
+        return `
+            <div class="card">
+                <h4 class="card__title">${film.title}</h4>
+                <img class="card__img" src=${film.img}></img>
+                <div class="card__info-container">
+                    <p class="card__info-title">EPISODE</p>
+                    <p class="card__info">${film.episode}</p>
+                    <p class="card__info-title">DIECTOR</p>
+                    <p class="card__info">${film.director}</p>
+                    <p class="card__info-title">DATE</p>
+                    <p class="card__info">${film.date}</p>
+                    <a class="card__link" href="#">+ MORE DETAILS</a>
+                </div>
+            </div>
+        `
+    }).join('')
+
+    return templatesFilms;
+}
+const addEventsToFilmLinks = (films) => {
+    let cardLinks = [...document.getElementsByClassName('card__link')];
+    console.log(cardLinks)
+    cardLinks.forEach((element, i) => {
+        element.addEventListener('click', () => {
+            console.log('Tengo que ir al detalle de:', films[i].urlDetail)
+        })
+    })
+}
 const printFilms = () => {
-    mainContainer.innerHTML = `
-    <section class="section">
+    mainContainer.innerHTML = "";
+    getFilms().then(response => {
+        let filmsCards = formatFilmsCards(response);
+
+        mainContainer.innerHTML = `
+     <section class="section">
         <h3 class="section__title">FILMS</h3>
         <section class="section__container">
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>
-            <div class="card">
-                <h4 class="card__title">LA AMENAZA FANTASMA</h4>
-                <img class="card__img" src="assets/images/films/1.jpg"></img>
-                <div class="card__info-container">
-                    <p class="card__info-title">EPISODE</p>
-                    <p class="card__info">I</p>
-                    <p class="card__info-title">DIECTOR</p>
-                    <p class="card__info">George Lucas</p>
-                    <p class="card__info-title">DATE</p>
-                    <p class="card__info">1977-01-05</p>
-                    <a class="card__link" href="#">+ MORE DETAILS</a>
-                </div>
-            </div>        
+           ${filmsCards}
         </section>
     </section>
     `;
-
-    getFilms().then(response => {
-        console.log(response);
-    })
+    addEventsToFilmLinks(response);
+    });
 }
