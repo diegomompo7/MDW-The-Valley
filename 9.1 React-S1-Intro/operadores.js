5f3a19e3a6f9fcd51d1061f03b6fb28a6284da6f
diegomompo <diego@diegomompo.com>
2023-02-14 10:57:39 +0100

    9.1 React

---
+++ 9.1 React-S1-Intro/operadores.js
@@ -0,0 +1,11 @@
+// Operador ||
+console.log(false || true) //true
+console.log(true || false) //true
+console.log(true ||  "string") //true
+console.log("string" || "string") //string
+
+// Operador &&
+console.log(false && true) //true
+console.log(true && false) //true
+console.log(true &&  "string") //true
+console.log("string" && "string") //string
