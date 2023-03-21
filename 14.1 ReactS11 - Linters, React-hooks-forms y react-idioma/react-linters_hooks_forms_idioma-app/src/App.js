18ec8cc9da35b1ec33fdc3cdc8a54737d8bbf444
Diego Mompó Redoli <diego.mompo.redoli@gmail.com>
2023-03-21 21:50:26 +0100

    14.1 ReactS11

--- 14.1 ReactS11 - Linters, React-hooks-forms y react-idioma/react-linters_hooks_forms_idioma-app/src/App.js
+++ 14.1 ReactS11 - Linters, React-hooks-forms y react-idioma/react-linters_hooks_forms_idioma-app/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import "./App.scss";
+import './App.scss';
+import RegisterForm from './components/Registrer';
 
 function App() {
   return (
     <div className='App'>
       <header className='App__header'>
-        <img src={logo} className='App__logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App__link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <RegisterForm></RegisterForm>
       </header>
     </div>
   );
---
+++ 14.1 ReactS11 - Linters, React-hooks-forms y react-idioma/react-linters_hooks_forms_idioma-app/src/components/Registrer.js
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form';
+import React from 'react';
+
+const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm();
+  const [formData, setFormData] = React.useState(null);
+
+  const onSubmit = (data) => setFormData(data);
+
+  return (
+    <div className='register-form'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder='First name' type='text' {...register('firstName', { required: true })} />
+          {errors?.firstName && <p className='register-form__error'>Este campo es obligatorio</p>}
+        </fieldset>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder='Last name' type='text' {...register('lastName', { required: true, minLength: 3 })} />
+          {errors?.lastName && <p className='register-form__error'>Este campo es obligatorio y debe tener al menos 3 caracteres</p>}
+        </fieldset>
+        <button type='submit'>Aceptar</button>
+      </form>
+
+      <h2>Datos del formulario:</h2>
+      <p>First name: {formData?.firstName}</p>
+      <p>Last name: {formData?.lastName}</p>
+
+      <h2>Datos del formulario en caliente:</h2>
+      <p>First name: {watch('firstName')}</p>
+      <p>Last name: {watch('lastName')}</p>
+    </div>
+  );
+};
+
+export default RegisterForm;
