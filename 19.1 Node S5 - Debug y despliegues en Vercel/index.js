39dd9c5881706723d30de68149f58a28734742cc
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-05-02 21:38:03 +0200

    aCT

--- 19.1 Node S5 - Debug y despliegues en Vercel/index.js
+++ 19.1 Node S5 - Debug y despliegues en Vercel/index.js
@@ -2,30 +2,34 @@ const express = require("express");
 const { userRouter } = require("./routes/user.routes.js");
 const { carRouter } = require("./routes/car.routes.js");
 
-// Conexión a la BBDD
-const { connect } = require("./db.js");
-connect();
+const main = async () => {
+  // Conexión a la BBDD
+  const { connect } = require("./db.js");
+  await connect();
 
-// Configuración del server
-const PORT = 3000;
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+  // Configuración del server
+  const PORT = 3000;
+  const server = express();
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
 
-// Rutas
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Esta es la home de nuestra API");
-});
-router.get("*", (req, res) => {
-  res.status(404).send("Lo sentimos :( No hemos encontrado la página solicitada.");
-});
+  // Rutas
+  const router = express.Router();
+  router.get("/", (req, res) => {
+    res.send("Esta es la home de nuestra API");
+  });
+  router.get("*", (req, res) => {
+    res.status(404).send("Lo sentimos :( No hemos encontrado la página solicitada.");
+  });
 
-// Usamos las rutas
-server.use("/user", userRouter);
-server.use("/car", carRouter);
-server.use("/", router);
+  // Usamos las rutas
+  server.use("/user", userRouter);
+  server.use("/car", carRouter);
+  server.use("/", router);
 
-server.listen(PORT, () => {
-  console.log(`Server levantado en el puerto ${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`Server levantado en el puerto ${PORT}`);
+  });
+};
+
+main();
